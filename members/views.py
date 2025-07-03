bf42362939a3caff88b20e03f4af1a5463e801be
Zabby <132018635+Zambagarrah@users.noreply.github.com>
2025-07-03 18:12:15 +0300

    Update views.py
    Updated Views for members' app

--- members/views.py
+++ members/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Member
 
 # Create your views here.
+def member_list(request):
+  members = Member.objects.all()
+  return render(request, 'members/member_list.html', {'members': members})
