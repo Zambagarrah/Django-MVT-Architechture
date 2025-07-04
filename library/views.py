fb013975ae1c67f19f2c5aa4d5a270cafd59ee82
Zabby <132018635+Zambagarrah@users.noreply.github.com>
2025-07-04 19:43:29 +0300

    Update views.py

--- library/views.py
+++ library/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Book
 # Create your views here.
 
-def book__list(request):
+def book_list(request):
   books = Book.objects.all()
   return render(request, 'library/book_list.html', {'books':books})
